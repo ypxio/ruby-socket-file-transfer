a7b004c144f54d16d8645b54da7392017840d48b
yuri <yuripertamax@gmail.com>
2012-10-02 17:34:05 +0700

    change path

--- client.rb
+++ client.rb
@@ -2,10 +2,10 @@
 require 'socket'
 
 host = '127.0.0.1'
-port = 6688
+port = 9999
 sock = TCPSocket.open(host, port)
 
-	data = sock.read
-	destFile = File.open('/home/yuripertamax/Public/pemjar2.txt', 'wb')
-	destFile.print data
-	destFile.close
+data = sock.read
+destFile = File.open('/path/to/saved/files', 'wb')
+destFile.print data
+destFile.close
--- server.rb
+++ server.rb
@@ -1,14 +1,14 @@
 #server sends client file over socket
 require 'socket'
-server = TCPServer.open(6671)
+server = TCPServer.open(9999)
 
 loop {
-  Thread.start(server.accept) do |client|
-    #client.puts(Time.now.ctime)
-	file = open('/home/yuripertamax/Music/animal.mp3', "rb")
-	fileContent = file.read
-	client.puts(fileContent)
-	client.close
+  	Thread.start(server.accept) do |client|
+	    #client.puts(Time.now.ctime)
+		file = open('/path/to/source/files', "rb")
+		fileContent = file.read
+		client.puts(fileContent)
+		client.close
 end
 
 }
